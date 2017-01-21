f634285871f4859c5e0f3c38d0e30f7f5dc35f51
talhoffer <nothing@dont.com>
2017-01-21 12:48:53 +0100

    refactoring image fetcher

--- imagefetch.py
+++ imagefetch.py
@@ -8,63 +8,58 @@ from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup
 
-STARTFROM = 'Olivia Peguero'
+def fetch_google_images(search_terms):
+    # ------------- set up only once for all search terms: -------------
+    driver = webdriver.Firefox()
+    driver.get("https://images.google.com")
+    # ------------------------------------------------------------------
 
-# ------------- set up: ----------
+    for search_term in search_terms:
+        elem = driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys(search_term)
+        elem.send_keys(Keys.RETURN)
 
-driver = webdriver.Firefox()
-driver.get("https://images.google.com")
+        while search_term not in driver.title:
+            time.sleep(.5)
 
-# --------------------------------
+        page_source = driver.page_source
 
-with open('artist_list.pickle', 'rb') as f:
-    artist_list = pickle.load(f)
+        # ------ from here on out all clean --------
 
-skip_number = artist_list.index(STARTFROM)
+        soup = BeautifulSoup(page_source, 'html.parser')
+        candidates = soup.find_all('div', class_="rg_meta")
 
-for artist_name in artist_list[skip_number:]:
-    elem = driver.find_element_by_name("q")
-    elem.clear()
-    elem.send_keys(artist_name)
-    elem.send_keys(Keys.RETURN)
+        image_urls = []
 
-    while artist_name not in driver.title:
-        time.sleep(1)
+        for tag in candidates:
+            crap = tag.string
+            regex = re.search(
+                'https?://.*?\.(png|jpg|jpeg|gif)\"',
+                crap,
+                re.IGNORECASE)
+            
+            if regex is not None:
+                first_match = crap[regex.start():regex.end()-1]
+                image_urls.append(first_match)
 
-    page_source = driver.page_source
+        for incr, url in enumerate(image_urls):
+            subdir = search_term.replace(' ', '_')
+            folder = os.path.join('images', subdir)
+            os.system(f"mkdir -p {folder}")
 
-    # ------ from here on out all clean --------
+            file_ending = url[url.rfind('.'):]
+            filename = str(incr).zfill(3) + file_ending
+            file = os.path.join(folder, filename)
+            # os.system(f"wget {url} -O {file}")
+            # os.system(f"aria2c {url} -o {file}")
+            cmd = ['aria2c', url, '-o', file,
+                   '--max-tries=2',
+                   '--retry-wait=1',
+                   '--connect-timeout=5',
+                   '--timeout=20',]
+            # subprocess is better:
+            subprocess.call(cmd)
 
-    soup = BeautifulSoup(page_source, 'html.parser')
-    candidates = soup.find_all('div', class_="rg_meta")
-
-    image_urls = []
-
-    for tag in candidates:
-        crap = tag.string
-        regex = re.search('https?://.*?\.(png|jpg|jpeg|gif)\"', crap, re.IGNORECASE)
-        if regex is not None:
-            first_match = crap[regex.start():regex.end()-1]
-            image_urls.append(first_match)
-
-    for incr, url in enumerate(image_urls):
-        subdir = artist_name.replace(' ', '_')
-        folder = os.path.join('images', subdir)
-        os.system(f"mkdir -p {folder}")
-
-        file_ending = url[url.rfind('.'):]
-        filename = str(incr).zfill(3) + file_ending
-        file = os.path.join(folder, filename)
-        # os.system(f"wget {url} -O {file}")
-        # os.system(f"aria2c {url} -o {file}")
-        cmd = ['aria2c', url, '-o', file,
-               '--max-tries=2',
-               '--retry-wait=1',
-               '--connect-timeout=5',
-               '--timeout=20',]
-        # subprocess is better:
-        subprocess.call(cmd)
-
-
-# aaaall the way at the end: 
-driver.close()
+    # aaaall the way at the end:
+    driver.close()
