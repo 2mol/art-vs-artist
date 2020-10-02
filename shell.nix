6decd377e5236991f4986799bb8eb6d6b42c2c1f
2mol <2mol@users.noreply.github.com>
2020-10-02 07:35:22 +0200

    revive

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+# Pro mode:
+# { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2c0034d5fbcb1b6b511a2369329a2446c84bf884.tar.gz") {}}:
+
+with pkgs; mkShell {
+  buildInputs = [
+    toilet
+
+    python38
+    # the rule is: python *tooling* is added here,
+    # while *libraries* are added through a python package manager.
+    python38Packages.jupyter 
+    python38Packages.poetry
+
+    chromedriver
+  ];
+
+  shellHook = ''
+    toilet " art versus artist " -f term -F border --gay
+  '';
+}
